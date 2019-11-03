40baf3442ac6b965a6646f2a629c4efeaa831216
Jacob Ferrero <jacobaferrero@gmail.com>
2019-11-02 22:31:03 -0500

    Fantasy updates

--- app/src/main/java/com/queatz/fantasydating/MainActivity.kt
+++ app/src/main/java/com/queatz/fantasydating/MainActivity.kt
@@ -2,19 +2,23 @@ package com.queatz.fantasydating
 
 import android.animation.Animator
 import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.graphics.Color
 import android.graphics.PointF
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.view.*
-import android.widget.Button
-import android.widget.TextView
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.toBitmap
+import androidx.core.graphics.get
 import androidx.core.view.GestureDetectorCompat
 import coil.Coil
 import coil.api.load
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.math.abs
+import kotlin.math.min
 import kotlin.math.sin
 
 
@@ -28,6 +32,7 @@ class MainActivity : AppCompatActivity() {
             field = value
 
             if (value) {
+                closeBub(bub3)
                 swipeUpArrow.rotation = 180f
                 fantasy.visibility = View.VISIBLE
                 storyText.visibility = View.GONE
@@ -59,11 +64,15 @@ class MainActivity : AppCompatActivity() {
             discoveryPreferencesLayout.visibility = if (value) View.VISIBLE else View.GONE
             showFeed = !value
 
-            if (!value) {
+            if (value) {
+                closeBub(bub2)
+            } else {
                 editPreferenceText.visibility = View.GONE
                 editProfileText.visibility = View.VISIBLE
             }
         }
+
+    val disposables = CompositeDisposable()
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -74,35 +83,77 @@ class MainActivity : AppCompatActivity() {
 
         background.setBackgroundColor(resources.getColor(R.color.colorPrimaryDark, theme))
 
+        fantasyText.movementMethod = ScrollingMovementMethod()
+
         notification1.text = resources.getString(R.string.link, "YOU MATCHED WITH AMY")
         notification2.text = resources.getString(R.string.link, "JING SENT YOU 3 MESSAGES")
         notification3.text = resources.getString(R.string.link, "YOU MATCHED WITH JING")
         notification4.text = resources.getString(R.string.link, "WELCOME TO FANTASY DATING")
 
-        stories.exitObservable
+        disposables.add(stories.exitObservable
             .subscribeOn(AndroidSchedulers.mainThread())
-            .subscribe { stories.start() }
+            .subscribe {
+                if (it == 1) {
+                    stories.start()
+                } else {
+                    stories.set(6)
+                    stories.next()
+                }
+            })
+
+        stories.count = 8
 
         Coil.load(this, "https://lh3.googleusercontent.com/80hJcieOULQhfT2hLS689_tNOCACzpilOYjTMvgw8aHH12Nk4hj7eTCsFdWY4lcC8laMoSAk8YIshlWMxRHELXYBE3UtDtWCK1_1uXFotpeUKn_D2AA0ZMcpQDwML8rBgDjMmFjaeW8")
         Coil.load(this, "https://lh3.googleusercontent.com/LE1DiS1zLJ-kdc639XxdC89NtNjBl3v8M7a2A_KX-8PaINqGvruYkcAnxOYu6Pbaa9asAdT75nHniyMUZRMrlSMoV0hH374dJlRdWzXhagh6ywZKvBZILyFEQnFLsnHLgIXQklUtcFo")
         Coil.load(this, "https://lh6.googleusercontent.com/7HwiyGQgVsnaPSi0KDp_IE-UqKaDmZIlQQqKyJXAXHwiyVoYfkQfQwbCMGMf3nHhC3sIKzDIaSJq2-Aod9RErfPZlCVoLrHU4kAt3K7rwPXbKPW7Js4FmY4KXoIwTt4asEm6bBGvBkk")
+        Coil.load(this, "https://lh3.googleusercontent.com/c6PnVBA7rKB_ofPEmcRXEQfNste2x5C3M5rmC1dlUUnoqHLlHg4R8wNZAS7LKFSBmL9f-5lB85oPwlBQ7Ib9aXTxHnHS9iBwdCkMQhTk4rgMsaKQyfObPg70xGB2_kH9GM8A8BOereo")
+        Coil.load(this, "https://lh4.googleusercontent.com/luVnq4laYiTHUcQG3mEWirhNy6mJJ_aSTlYWcKHTfDFpQJOCKALKFUgJjJEQWeeadVtjv663soNxDSfm29Awgr2eYMDyDuHwMGUIOKho6zHMK-90FGR3Bs4ZMZYMSGmTk3Al58jcyKI")
+        Coil.load(this, "https://lh3.googleusercontent.com/F7BDjvSURaJuIEYUZ3ikrK6rdvqXjYmZvPrFoyyNpoHOAVOlzcheVO9WFCB3bZgkcvuhNxEQdKFiC-SGKOIDPdc8rPmHrTZFc6OovxKiM9VMcx93Bdf6kKMaZxFmVobxxZyXSh3kqfI")
+        Coil.load(this, "https://lh5.googleusercontent.com/ecnggpWNeMRffzCDrEugrJqbolVoOXyd_jyHSRtuezPI461GIxdOXF6_e2yGE5Yf9BWf5QeSFwtaw8fsWdn8uKdyOtCZQDh7N86bNL5yXs7XsYHNJjpxUJoCsjdF2I1cTpBTwJuKLLs")
+        Coil.load(this, "https://lh5.googleusercontent.com/Z7HKvSPvB-yj3xqvu1W8pOaHhzwS0uFVw7l6OoqDmMpBb4FZOZVkKCIFxB2T2mELkkQOXkfO4nAafk06-yGAO_zk22SLyTlgxW4RZAUjwphApCxu2i1CPbdXyG9ojVAa94yYkf3jwjA")
 
-        stories.currentObservable
+        disposables.add(stories.currentObservable
+            .subscribeOn(AndroidSchedulers.mainThread())
             .subscribe {
-                background.load(
-                    when (it) {
-                        0 -> "https://lh3.googleusercontent.com/80hJcieOULQhfT2hLS689_tNOCACzpilOYjTMvgw8aHH12Nk4hj7eTCsFdWY4lcC8laMoSAk8YIshlWMxRHELXYBE3UtDtWCK1_1uXFotpeUKn_D2AA0ZMcpQDwML8rBgDjMmFjaeW8"
-                        1 -> "https://lh3.googleusercontent.com/LE1DiS1zLJ-kdc639XxdC89NtNjBl3v8M7a2A_KX-8PaINqGvruYkcAnxOYu6Pbaa9asAdT75nHniyMUZRMrlSMoV0hH374dJlRdWzXhagh6ywZKvBZILyFEQnFLsnHLgIXQklUtcFo"
-                        else -> "https://lh6.googleusercontent.com/7HwiyGQgVsnaPSi0KDp_IE-UqKaDmZIlQQqKyJXAXHwiyVoYfkQfQwbCMGMf3nHhC3sIKzDIaSJq2-Aod9RErfPZlCVoLrHU4kAt3K7rwPXbKPW7Js4FmY4KXoIwTt4asEm6bBGvBkk"
-                    }
-                )
+                val photo = when (it) {
+                    0 -> "https://lh3.googleusercontent.com/80hJcieOULQhfT2hLS689_tNOCACzpilOYjTMvgw8aHH12Nk4hj7eTCsFdWY4lcC8laMoSAk8YIshlWMxRHELXYBE3UtDtWCK1_1uXFotpeUKn_D2AA0ZMcpQDwML8rBgDjMmFjaeW8"
+                    1 -> "https://lh3.googleusercontent.com/LE1DiS1zLJ-kdc639XxdC89NtNjBl3v8M7a2A_KX-8PaINqGvruYkcAnxOYu6Pbaa9asAdT75nHniyMUZRMrlSMoV0hH374dJlRdWzXhagh6ywZKvBZILyFEQnFLsnHLgIXQklUtcFo"
+                    2 -> "https://lh6.googleusercontent.com/7HwiyGQgVsnaPSi0KDp_IE-UqKaDmZIlQQqKyJXAXHwiyVoYfkQfQwbCMGMf3nHhC3sIKzDIaSJq2-Aod9RErfPZlCVoLrHU4kAt3K7rwPXbKPW7Js4FmY4KXoIwTt4asEm6bBGvBkk"
+                    3 -> "https://lh3.googleusercontent.com/c6PnVBA7rKB_ofPEmcRXEQfNste2x5C3M5rmC1dlUUnoqHLlHg4R8wNZAS7LKFSBmL9f-5lB85oPwlBQ7Ib9aXTxHnHS9iBwdCkMQhTk4rgMsaKQyfObPg70xGB2_kH9GM8A8BOereo"
+                    4 -> "https://lh4.googleusercontent.com/luVnq4laYiTHUcQG3mEWirhNy6mJJ_aSTlYWcKHTfDFpQJOCKALKFUgJjJEQWeeadVtjv663soNxDSfm29Awgr2eYMDyDuHwMGUIOKho6zHMK-90FGR3Bs4ZMZYMSGmTk3Al58jcyKI"
+                    5 -> "https://lh3.googleusercontent.com/F7BDjvSURaJuIEYUZ3ikrK6rdvqXjYmZvPrFoyyNpoHOAVOlzcheVO9WFCB3bZgkcvuhNxEQdKFiC-SGKOIDPdc8rPmHrTZFc6OovxKiM9VMcx93Bdf6kKMaZxFmVobxxZyXSh3kqfI"
+                    6 -> "https://lh5.googleusercontent.com/ecnggpWNeMRffzCDrEugrJqbolVoOXyd_jyHSRtuezPI461GIxdOXF6_e2yGE5Yf9BWf5QeSFwtaw8fsWdn8uKdyOtCZQDh7N86bNL5yXs7XsYHNJjpxUJoCsjdF2I1cTpBTwJuKLLs"
+                    else -> "https://lh5.googleusercontent.com/Z7HKvSPvB-yj3xqvu1W8pOaHhzwS0uFVw7l6OoqDmMpBb4FZOZVkKCIFxB2T2mELkkQOXkfO4nAafk06-yGAO_zk22SLyTlgxW4RZAUjwphApCxu2i1CPbdXyG9ojVAa94yYkf3jwjA"
+                }
+
+                background.load(photo)
+
+                Coil.load(this, photo) {
+                    allowHardware(false)
+                    target({}, {}, { drawable ->
+                        val color = drawable.toBitmap(4, 4, Bitmap.Config.ARGB_8888)[0, 3]
+                        val hsv = floatArrayOf(0f, 0f, 0f)
+                        Color.colorToHSV(color, hsv)
+                        hsv[2] = min(hsv[2], .333f)
+                        val c = Color.HSVToColor(hsv)
+                        storyText.setTextColor(c)
+                        fantasy.setBackgroundColor(Color.argb(163, Color.red(c) / 4, Color.green(c) / 4, Color.blue(c) / 4))
+                    })
+                }
 
                 storyText.text = when (it) {
                     0 -> "Emi, 23<br /><br />I love pretending I’m visiting Earth on an intergalactic mission."
                     1 -> "Sal, 27<br /><br />I love meeting new people in the rush of the city."
-                    else -> "Liz, 29<br /><br />I love taking photos of myself in the pool!"
+                    2 -> "Liz, 29<br /><br />I love taking photos of myself in the pool!"
+                    3 -> "Mary, 24<br /><br /> love going out for a run in the cool evening."
+                    else -> "Sal, 27<br /><br />I love meeting new people out in the countryside."
                 }
-            }
+
+                fantasyText.text = when (it) {
+                    0 -> "I want a boy to masturbate and kiss me on a bench overlooking the lake.\n\nEmi’s Fantasy\n\nI want a boy to masturbate and kiss me on a bench overlooking the lake.\n\nEmi’s Fantasy\n\nI want a boy to masturbate and kiss me on a bench overlooking the lake.\n\nEmi’s Fantasy\n\nI want a boy to masturbate and kiss me on a bench overlooking the lake.\n\nEmi’s Fantasy\n\nI want a boy to masturbate and kiss me on a bench overlooking the lake.\n\nEmi’s Fantasy\n\nI want a boy to masturbate and kiss me on a bench overlooking the lake.\n\nEmi’s Fantasy\n\nI want a boy to masturbate and kiss me on a bench overlooking the lake.\n\nEmi’s Fantasy\n\nI want a boy to masturbate and kiss me on a bench overlooking the lake.\n\n"
+                    else -> "I want a boy to masturbate and kiss me on a bench overlooking the lake."
+                }
+            })
 
         val gestures = object : GestureDetector.SimpleOnGestureListener() {
             override fun onFling(
@@ -186,6 +237,7 @@ class MainActivity : AppCompatActivity() {
 
             if (showFeed) {
                 stories.start()
+                closeBub(bub1)
             } else {
                 stories.next()
             }
@@ -267,8 +319,9 @@ class MainActivity : AppCompatActivity() {
 
             bub1.visibility = View.VISIBLE
             bub2.visibility = View.VISIBLE
+            bub3.visibility = View.VISIBLE
 
-            listOf(bub1, bub2).forEach {
+            listOf(bub1, bub2, bub3).forEach {
                 it.alpha = 0f
                 it.animate()
                     .alpha(1f)
@@ -279,7 +332,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun showAustinOnly() {
-        showModal()
     }
 
     private fun updateDiscoveryPreferences() {
@@ -310,29 +362,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun showModal() {
-        AlertDialog.Builder(this)
-            .setCancelable(false)
-            .setMessage(getString(R.string.location_modal_message))
-            .setPositiveButton(getString(R.string.enable_location)) { _, _ ->
-
-            }
-            .show().apply {
-                val messageTextView = window?.findViewById(android.R.id.message) as TextView
-                val positiveButton = window?.findViewById(android.R.id.button1) as Button
-
-                val pad = resources.getDimensionPixelSize(R.dimen.pad)
-
-                positiveButton.setBackgroundResource(android.R.color.transparent)
-                positiveButton.setPaddingRelative(pad, 0, pad, 0)
-
-                positiveButton.setOnClickListener { dismiss() }
-
-                messageTextView.setTextAppearance(R.style.Text_Medium)
-                messageTextView.setLineSpacing(0f, 1.4f)
-            }
-    }
-
     private fun zoom() {
         val x = 0.5f + 0.5f * sin((System.currentTimeMillis().toDouble() % 30000) / 30000.0 * Math.PI * 2).toFloat()
         background.origin = PointF(x, x)
@@ -344,6 +373,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun closeBub(view: View) {
+
         view.animate()
             .alpha(0f)
             .setDuration(200)
@@ -353,7 +383,7 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 override fun onAnimationEnd(p0: Animator?) {
-                    (view.parent as ViewGroup).removeView(view)
+                    (view.parent as ViewGroup?)?.removeView(view)
                 }
 
                 override fun onAnimationCancel(p0: Animator?) {
